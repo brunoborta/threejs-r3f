3705370750748484f0470363f9630a471e462ff1
Bruno Bortagaray <bruno.bortagaray@highsnobiety.com>
2023-09-22 16:53:38 +0200

    feat: 3d text with r3f

---
+++ 48-3d-text-with-r3f/src/Experience.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+import {
+  Text3D,
+  OrbitControls,
+  Center,
+  useMatcapTexture,
+  Torus,
+} from "@react-three/drei";
+import * as THREE from "three";
+import { Perf } from "r3f-perf";
+import { useFrame } from "@react-three/fiber";
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32);
+const material = new THREE.MeshMatcapMaterial();
+
+export default function Experience() {
+  // const [torus, setTorus] = useState();
+  // const [material, setMaterial] = useState();
+
+  const donutGroup = useRef();
+  // const donuts = useRef([]);
+  const [matcapTexture] = useMatcapTexture("7B5254_E9DCC7_B19986_C8AC91", 256);
+
+  useFrame((_, delta) => {
+    // Group solution - simpler
+    donutGroup.current.children.forEach((donut) => {
+      donut.rotation.y += delta * 0.25;
+    });
+
+    // Adding an array of refs to the donuts ref - more complex
+    // donuts.current.forEach((donut) => {
+    //   donut.rotation.y += delta * 0.25;
+    // });
+  });
+
+  useEffect(() => {
+    matcapTexture.colorSpace = THREE.SRGBColorSpace;
+    matcapTexture.needsUpdate = true;
+
+    material.matcap = matcapTexture;
+    material.needsUpdate = true;
+  }, []);
+
+  return (
+    <>
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      {/* <torusGeometry ref={setTorus} args={[1, 0.6, 16, 32]} />
+      <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
+
+      <Center>
+        <Text3D
+          font="./fonts/helvetiker_regular.typeface.json"
+          size={0.75}
+          height={0.2}
+          curveSegments={12}
+          bevelEnabled
+          bevelThickness={0.02}
+          bevelSize={0.02}
+          bevelOffset={0}
+          bevelSegments={5}
+          material={material}
+        >
+          Hello R3F
+        </Text3D>
+      </Center>
+      <group ref={donutGroup}>
+        {Array.from({ length: 100 }).map((_, i) => (
+          <mesh
+            // ref={(ref) => (donuts.current[i] = ref)}
+            key={i}
+            geometry={torusGeometry}
+            material={material}
+            position={[
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10,
+              (Math.random() - 0.5) * 10,
+            ]}
+            scale={0.25 + Math.random() * 0.25}
+            rotation={[Math.random() * Math.PI, Math.random() * Math.PI, 0]}
+          />
+        ))}
+      </group>
+    </>
+  );
+}
