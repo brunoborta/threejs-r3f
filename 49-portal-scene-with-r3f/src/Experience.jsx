9baf1ab5f2398b0d95c51b137291fa9ad0ea6954
Bruno Bortagaray <bruno.bortagaray@highsnobiety.com>
2023-09-23 16:01:18 +0200

    feat: Portal scene

---
+++ 49-portal-scene-with-r3f/src/Experience.jsx
@@ -0,0 +1,78 @@
+import {
+  useGLTF,
+  OrbitControls,
+  useTexture,
+  Center,
+  Sparkles,
+  shaderMaterial,
+} from "@react-three/drei";
+import * as THREE from "three";
+
+import portalVertexShader from "./shaders/portal/vertex.glsl";
+import portalFragmentShader from "./shaders/portal/fragment.glsl";
+import { extend, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const PortalMaterial = shaderMaterial(
+  {
+    uTime: 0,
+    uColorStart: new THREE.Color("#ffffff"),
+    uColorEnd: new THREE.Color("#000000"),
+  },
+  portalVertexShader,
+  portalFragmentShader
+);
+
+extend({ PortalMaterial });
+
+export default function Experience() {
+  const { nodes } = useGLTF("./model/portal.glb");
+  const bakedTexture = useTexture("./model/baked.jpg");
+  const portalMaterialRef = useRef();
+
+  useFrame((_, delta) => {
+    const portalMaterial = portalMaterialRef.current;
+
+    portalMaterial.uTime += delta;
+  });
+
+  return (
+    <>
+      <color attach="background" args={["#201919"]} />
+      <OrbitControls makeDefault />
+
+      <Center>
+        <mesh geometry={nodes.baked.geometry}>
+          <meshBasicMaterial map={bakedTexture} map-flipY={false} />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightA.geometry}
+          position={nodes.poleLightA.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={nodes.poleLightB.geometry}
+          position={nodes.poleLightB.position}
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+        <mesh
+          geometry={nodes.portalLight.geometry}
+          position={nodes.portalLight.position}
+          rotation={nodes.portalLight.rotation}
+        >
+          <portalMaterial ref={portalMaterialRef} />
+        </mesh>
+
+        <Sparkles
+          size={6}
+          scale={[4, 2, 4]}
+          position-y={1}
+          speed={0.2}
+          count={40}
+        />
+      </Center>
+    </>
+  );
+}
